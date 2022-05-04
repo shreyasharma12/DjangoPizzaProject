5a1958851b60ba9d4118d6886d57ab698326482f
Shreya Sharma <shreya_sharma1@baylor.edu>
2022-05-03 21:04:50 -0500

    Project Edits 5/3

--- pizzas/forms.py
+++ pizzas/forms.py
@@ -1,10 +1,17 @@
 from django import forms 
 
-from .models import Pizza
+from .models import Pizza, Topping
 
-class PizzaForm(forms.ModelForm):
+class CommentForm(forms.ModelForm):
     class Meta:
         model = Pizza
         fields = ['pizza_name']
         labels = {'pizza_name':''}
+
+class NewCommentForm(forms.ModelForm):
+    class Meta:
+        model = Topping
+        fields = ['pizza_name']
+        labels = {'pizza_name':''}
+        widgets = {'pizza_name': forms.Textarea(attrs={'cols':80})}
 
